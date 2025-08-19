53362d11565752964e3f6cf4b4ee270fb69d8a69
muhdafif0508 <muhdafif0508@gmail.com>
2025-08-20 02:43:24 +0800

    add code

--- server.js
+++ server.js
@@ -3,21 +3,31 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt      = require('bcrypt');
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  if (err) throw err;
+  console.log("Async hash:", hash);
 
-
+  bcrypt.compare(myPlaintextPassword, hash, (err, result) => {
+    if (err) throw err;
+    console.log("Async compare result:", result);
+  });
+});
 
 //END_ASYNC
 
 //START_SYNC
+const hashSync = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log("Sync hash:", hashSync);
 
-
+const resultSync = bcrypt.compareSync(myPlaintextPassword, hashSync);
+console.log("Sync compare result:", resultSync);
 
 //END_SYNC
 
